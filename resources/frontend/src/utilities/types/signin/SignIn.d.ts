c881e5736dc41fa90b59d0c1f3142b6ed17f344c
thushan <thushannirmal@gmail.com>
2025-06-22 21:22:47 +0530

    Create from functionalities  with type definition

---
+++ resources/frontend/src/utilities/types/signin/SignIn.d.ts
@@ -0,0 +1,13 @@
+import React from "react";
+
+export interface ISignInState {
+    email: string,
+    password: string,
+}
+
+export interface ISignInFormProp {
+    handleInputField: (event: React.ChangeEvent<HTMLInputElement>) => void
+    handleSubmit:(event:React.FormEvent) => Promise<void>
+
+}
+
